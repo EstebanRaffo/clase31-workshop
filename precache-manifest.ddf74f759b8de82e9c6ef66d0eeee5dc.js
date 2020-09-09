60d78944a62ff0f231edfcf9567c524b335beb81
Esteban Raffo <e.fraffo@gmail.com>
2020-09-09 20:49:15 -0300

    Updates

---
+++ precache-manifest.ddf74f759b8de82e9c6ef66d0eeee5dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e51058c98dfeb3701d1518516eedf6",
+    "url": "/clase31-workshop/index.html"
+  },
+  {
+    "revision": "ab35a02244cb34ca9886",
+    "url": "/clase31-workshop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1bc9d9a23471ee2961f",
+    "url": "/clase31-workshop/static/js/2.653b8a97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clase31-workshop/static/js/2.653b8a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab35a02244cb34ca9886",
+    "url": "/clase31-workshop/static/js/main.aaa87b7f.chunk.js"
+  },
+  {
+    "revision": "2c14729161b197d83cec",
+    "url": "/clase31-workshop/static/js/runtime-main.3352d05e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clase31-workshop/static/media/logo.5d5d9eef.svg"
+  }
+]);
